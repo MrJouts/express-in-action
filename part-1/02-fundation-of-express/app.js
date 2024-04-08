0a0e217f20eb3771b8d10f58025aaae432cc7264
mrjouts <es.hotes@gmail.com>
2024-04-08 01:10:53 -0300

    Part 1 - Logging middleware

--- part-1/02-fundation-of-express/app.js
+++ part-1/02-fundation-of-express/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const http = require("http");
-
 var app = express();
 
+app.use((request, response, next) => {
+  console.log("In comes a " + request.method + " to " + request.url);
+  next();
+});
+
 app.use((request, response) => {
-  console.log("In comes a request to: " + request.url);
+  response.writeHead(200, { "Content-Type": "text/plain" });
   response.end("Hello, world!");
 });
 
