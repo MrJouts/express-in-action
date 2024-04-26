61df0bb80cf2dbc4fb3389a7b70a3ecc52e66712
Emiliano Hotes <emiliano.hotes@despegar.com>
2024-04-26 06:51:15 -0300

    Part 2 - 404 handler middleware

--- part-2/01-middleware/static-file-server/app.js
+++ part-2/01-middleware/static-file-server/app.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
 const app = express();
 
 app.use((req, res, next) => {
-  console.log('Request IP: ' + req.url);
-  console.log('Request date: ' + new Date());
+  console.log("Request IP: " + req.url);
+  console.log("Request date: " + new Date());
   next();
 });
 
 app.use((req, res, next) => {
-  var filePath = path.join(__dirname, 'static', req.url);
+  var filePath = path.join(__dirname, "static", req.url);
   fs.stat(filePath, (err, fileInfo) => {
     if (err) {
       next();
@@ -26,6 +26,11 @@ app.use((req, res, next) => {
   });
 });
 
+app.use((req, res) => {
+  res.status(404);
+  res.send("File not found!");
+});
+
 app.listen(3000, () => {
-  console.log('App starte on port 3000');
+  console.log("App starte on port 3000");
 });
