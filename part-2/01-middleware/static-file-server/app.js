390b80ebebda5f8bb7e8767650f0d2b373831859
Emiliano Hotes <emiliano.hotes@despegar.com>
2024-04-26 06:43:42 -0300

    Part 2 - The static file server middleware

--- part-2/01-middleware/static-file-server/app.js
+++ part-2/01-middleware/static-file-server/app.js
@@ -1,15 +1,31 @@
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
 
 app.use((req, res, next) => {
-    console.log("Request IP: " + req.url);
-    console.log("Request date: " + new Date());
-    next();
+  console.log('Request IP: ' + req.url);
+  console.log('Request date: ' + new Date());
+  next();
+});
+
+app.use((req, res, next) => {
+  var filePath = path.join(__dirname, 'static', req.url);
+  fs.stat(filePath, (err, fileInfo) => {
+    if (err) {
+      next();
+      return;
+    }
+
+    if (fileInfo.isFile()) {
+      res.sendFile(filePath);
+    } else {
+      next();
+    }
+  });
 });
 
 app.listen(3000, () => {
-    console.log("App starte on port 3000");
+  console.log('App starte on port 3000');
 });
