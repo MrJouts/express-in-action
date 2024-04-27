d1d9f528fe9d9f68537abc846d77b4863250bc73
Emiliano Hotes <emiliano.hotes@despegar.com>
2024-04-26 23:48:06 -0300

    Part 2 - Grabbing parameters to routes and using regular expressions

--- part-2/02-routing/app.js
+++ part-2/02-routing/app.js
@@ -6,6 +6,26 @@ app.get("/olivia", (req, res) => {
   res.send("Welcome to Olivia's homepage!");
 });
 
+// app.get("/users/:userid", (req, res) => {
+//   const userId = parseInt(req.params.userid, 10);
+//   console.log("User id: " + userId);
+//   res.end();
+// });
+
+// app.get(/^\/users\/(\d+)$/, function (req, res) {
+//   var userId = parseInt(req.params[0], 10);
+//   console.log("User id: " + userId);
+//   res.end();
+// });
+
+app.get(/^\/users\/(\d+)-(\d+)$/, function (req, res) {
+  var startId = parseInt(req.params[0], 10);
+  var endId = parseInt(req.params[1], 10);
+  console.log("startId: " + startId);
+  console.log("endId: " + endId);
+  res.end();
+});
+
 app.use((req, res) => {
   res.status(404).send("Page not found!");
 });
